c34958d2cffbd4ceb6c54db6de9377207504fd03
Shiba-You <50415577+Shiba-You@users.noreply.github.com>
2021-09-22 22:13:41 +0900

    test

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,11 @@ import { ServerStyleSheets } from "@material-ui/styles";
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      });
+    // const originalRenderPage = ctx.renderPage;
+    // ctx.renderPage = () =>
+    //   originalRenderPage({
+    //     enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    //   });
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps,
       styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
--- pages/mypage.js
+++ pages/mypage.js
@@ -34,39 +34,28 @@ export default function Mypage(props) {
 
 Mypage.getInitialProps = async ({ req, res }) => {
   const isServerSide = typeof window === "undefined";
-
   if (isServerSide && req && res) {
     const root = "http://localhost:3000";
     const options = { headers: { cookie: req.headers.cookie || "" } };
-
     const result = await fetch(`${root}/api/loggedIn`, options);
     const json = (await result.json())
-
-
     if (!json.user) {
       res.writeHead(302, { Location: "/login" });
       res.end();
     }
-
     return { 
       email: (json.user || {}).email || ""
     };
-
   }
-
   if (!isServerSide) {
     const result = await fetch("./api/loggedIn");
     const json = (await result.json())
-   
     if (!json.user) Router.push({pathname: "/login"});
-
     return { 
       email: (json.user || {}).email || ""
     };
   }
-
   return { 
     email: ""
   };
-
 };
