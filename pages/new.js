198ad4160646750e272c014999ebb05a352907e3
Shiba-You <50415577+Shiba-You@users.noreply.github.com>
2021-09-26 12:51:59 +0900

    add Upload for new article

--- pages/new.js
+++ pages/new.js
@@ -1,15 +1,57 @@
-import Router, { useRouter } from "next/router";
+import Router from "next/router";
+import { useState } from "react";
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 import Layout from '../components/template/layout';
+import UpLoad from '../components/Upload';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '100px'
+  },
+}));
 
 export default function New(props) {
-  const { email } = props
+  const { currentUser } = props
+  const classes = useStyles();
   const title = "Add new book";
+  const [artileTitle, setArticleTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState("");
 
   return(
   <>
-    <Layout title={title} email={email}>
-      <p>New book</p>
+    <Layout title={title} currentUser={currentUser}>
+      <Grid container spacing={3} className={classes.root}>
+        <Grid container item xs={12}>
+          <TextField
+            className={classes.text}
+            label="name"
+            autoComplete="current-password"
+            variant="outlined"
+            margin="dense"
+            value={artileTitle}
+            onChange={(e) => setArticleTitle(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <UpLoad currentUser={currentUser} image={image} setImage={setImage} />
+        </Grid>
+        <Grid item xs={9}>
+          <TextField
+            id="outlined-textarea"
+            label="Multiline Placeholder"
+            placeholder="Placeholder"
+            multiline
+            variant="outlined"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Grid>
+      </Grid>
     </Layout>
   </>
   );
@@ -18,39 +60,28 @@ export default function New(props) {
 
 New.getInitialProps = async ({ req, res }) => {
   const isServerSide = typeof window === "undefined";
-
   if (isServerSide && req && res) {
     const root = "http://localhost:3000";
     const options = { headers: { cookie: req.headers.cookie || "" } };
-
     const result = await fetch(`${root}/api/loggedIn`, options);
     const json = (await result.json())
-
-
     if (!json.user) {
       res.writeHead(302, { Location: "/login" });
       res.end();
     }
-
     return { 
-      email: (json.user || {}).email || ""
+      currentUser: (json.user || {}).currentUser || ""
     };
-
   }
-
   if (!isServerSide) {
     const result = await fetch("./api/loggedIn");
     const json = (await result.json())
-   
     if (!json.user) Router.push({pathname: "/login"});
-
     return { 
-      email: (json.user || {}).email || ""
+      currentUser: (json.user || {}).currentUser || ""
     };
   }
-
   return { 
-    email: ""
+    currentUser: ""
   };
-
 };
